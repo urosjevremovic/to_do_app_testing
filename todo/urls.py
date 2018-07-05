66aafa8474bda102827698204e68146d7d1c626e
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-07-05 13:52:47 +0200

    added TestCases for home view and register view

--- todo/urls.py
+++ todo/urls.py
@@ -8,33 +8,33 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     # Home
-    url(r'^$', Home.as_view(), name = 'home'),
+    url(r'^$', Home.as_view(), name='home'),
 
     # Authentication and registration
-    url(r'^login/$', auth_views.login, name = 'login'),
-    url(r'^logout/$', auth_views.logout, name = 'logout'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^register/$', register, name='register'),
 
     # Task management
     url(r'^tasks/$',
-        login_required(tasks_views.TaskListView.as_view(), login_url = 'login'),
-        name = 'tasks'),
+        login_required(tasks_views.TaskListView.as_view(), login_url='login'),
+        name='tasks'),
 
     url(r'^task/$',
         login_required(tasks_views.NewTaskView.as_view()),
-        name = 'task-add'),
+        name='task-add'),
 
     url(r'^task/(?P<task_id>\d+)/$',
         login_required(tasks_views.TaskView.as_view()),
-        name = 'task-edit'),
+        name='task-edit'),
 
     url(r'^delete/(?P<task_id>\d+)/$',
         login_required(tasks_views.DeleteTaskView.as_view()),
-        name = 'task-delete'),
+        name='task-delete'),
 
     url(r'^toggle/(?P<task_id>\d+)/$',
         login_required(tasks_views.toggle_complete_view),
-        name = 'task-toggle'),
+        name='task-toggle'),
 
     # Django admin
     url(r'^admin/', admin.site.urls),
